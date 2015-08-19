f132c0ea831af1bc545550561cd58199fafd86d1
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:40:08 +0200

    Cleanup Perl examples

--- software/examples/perl/example_threshold.pl
+++ software/examples/perl/example_threshold.pl
@@ -10,12 +10,12 @@ use constant UID => 'XYZ'; # Change to your UID
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $co2 = Tinkerforge::BrickletCO2->new(&UID, $ipcon); # Create device object
 
-# Callback for CO2 concentration greater than 750 ppm
-sub cb_reached
+# Callback subroutine for CO2 concentration greater than 750 ppm (parameter has unit ppm)
+sub cb_co2_concentration_reached
 {
     my ($co2_concentration) = @_;
 
-    print "CO2 Concentration: ".$co2_concentration." ppm\n";
+    print "CO2 Concentration: " . $co2_concentration . " ppm\n";
 }
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
@@ -24,10 +24,10 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Get threshold callbacks with a debounce time of 10 seconds (10000ms)
 $co2->set_debounce_period(10000);
 
-# Register threshold reached callback to function cb_reached
-$co2->register_callback($co2->CALLBACK_CO2_CONCENTRATION_REACHED, 'cb_reached');
+# Register threshold reached callback to subroutine cb_co2_concentration_reached
+$co2->register_callback($co2->CALLBACK_CO2_CONCENTRATION_REACHED, 'cb_co2_concentration_reached');
 
-# Configure threshold for "greater than 750 ppm"
+# Configure threshold for "greater than 750 ppm" (unit is ppm)
 $co2->set_co2_concentration_callback_threshold('>', 750, 0);
 
 print "Press any key to exit...\n";
