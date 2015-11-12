3f6b2c1c71836b02a5de61f7e4302939edd778f7
Matthias Bolte <matthias@tinkerforge.com>
2015-11-12 15:22:31 +0100

    matlab: Fix callback handling for R2014a
    http://undocumentedmatlab.com/blog/matlab-callbacks-for-java-events-in-r2014a

--- software/examples/matlab/matlab_example_threshold.m
+++ software/examples/matlab/matlab_example_threshold.m
@@ -7,7 +7,7 @@ function matlab_example_threshold()
     UID = 'XYZ'; % Change to your UID
 
     ipcon = IPConnection(); % Create IP connection
-    co2 = BrickletCO2(UID, ipcon); % Create device object
+    co2 = handle(BrickletCO2(UID, ipcon), 'CallbackProperties'); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
