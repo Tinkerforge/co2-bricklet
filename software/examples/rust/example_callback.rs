42e2c2d2017a7679af5cbd221d0692edf2a77b16
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:51:57 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  co2_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{co2_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,23 +10,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let co2 = Co2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let co2_concentration_receiver = co2.get_co2_concentration_callback_receiver();
+    let co2_concentration_receiver = co2.get_co2_concentration_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `co2` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for co2_concentration in co2_concentration_receiver {           
-                		println!("CO2 Concentration: {} ppm", co2_concentration);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `co2` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for co2_concentration in co2_concentration_receiver {
+            println!("CO2 Concentration: {} ppm", co2_concentration);
+        }
+    });
 
-		// Set period for CO2 concentration receiver to 1s (1000ms).
-		// Note: The CO2 concentration callback is only called every second
-		//       if the CO2 concentration has changed since the last call!
-		co2.set_co2_concentration_callback_period(1000);
+    // Set period for CO2 concentration receiver to 1s (1000ms).
+    // Note: The CO2 concentration callback is only called every second
+    //       if the CO2 concentration has changed since the last call!
+    co2.set_co2_concentration_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  co2_bricklet::*};
-
+use tinkerforge::{co2_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,11 +11,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let co2 = Co2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get current CO2 concentration.
-let co2_concentration = co2.get_co2_concentration().recv()?;
-		println!("CO2 Concentration: {} ppm", co2_concentration);
+    // Get current CO2 concentration.
+    let co2_concentration = co2.get_co2_concentration().recv()?;
+    println!("CO2 Concentration: {} ppm", co2_concentration);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
